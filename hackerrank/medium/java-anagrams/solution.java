eba563d293e330571b2e6bc675dac349d7e3cdb0
yugandharamudalapalli-ops <yugandharamudalapalli@gmail.com>
2026-08-09 12:08:13 +0530

    ✅ Java Anagrams | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-09

---
+++ hackerrank/medium/java-anagrams/solution.java
@@ -0,0 +1,28 @@
+
+
+    static boolean isAnagram(String a, String b) {
+        // Complete the function
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < a.length(); i++) {
+            count[a.charAt(i) - 'a']++;
+            count[b.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    
+    }
+
