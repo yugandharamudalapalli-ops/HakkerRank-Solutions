05d701dbdf80d7fe56d98b9136c64e11f757b245
yugandharamudalapalli-ops <yugandharamudalapalli@gmail.com>
2026-08-11 10:59:19 +0530

    ✅ LJAAS20 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-11

---
+++ codechef/medium/LJAAS20/solution.java
@@ -0,0 +1,9 @@
+class Codechef {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            int result = (int) Math.pow(i, 2);
+            System.out.println(i + " - " + result);
+        }
+    }
+}
